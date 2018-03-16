29980574baea5168fc80ac578b86e8a12b19ddcb
William-L79 <willou9979@gmail.com>
2018-03-16 21:40:27 +0100

    Section_Home + BugNav

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,21 @@
-import { User } from './user';
+$(document).ready(function() {
 
-console.log('Welcome !');
+    $(".home__slide").css("display","flex").animate({left:100, opacity:1},2000);
 
-let name = 'Bastien';
-const user = new User(name);
+
+    $(window).scroll(function(){
+            if ($(this).scrollTop() > 700   ) {
+                $('.header').fadeIn(500,"swing");
+            } else {
+                $('.header').fadeOut(500,"swing");
+            }
+        });
+
+    $('.glissement').on('click', function() { // Au clic sur un élément
+            let page = $(this).attr('href'); // Page cible
+            let speed = 600; // Durée de l'animation (en ms)
+            $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+            return false;
+        });
+
+});
